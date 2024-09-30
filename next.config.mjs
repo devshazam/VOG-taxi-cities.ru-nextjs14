a196fb5c1fbc3fd82837535aae13cb224f2a02ba
devshazam <devshazam@gmail.com>
2024-09-30 13:11:03 +0300

    init

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+
+    output: 'export',
+    trailingSlash: true,
+    skipTrailingSlashRedirect: true,
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
+};
+
+export default nextConfig;
